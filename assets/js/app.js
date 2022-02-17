d8d256cac7a45e7c77c5a6aeb0d2bdd836349ef0
Tusar <tusarahmed141@gmail.com>
2022-02-17 15:50:01 +0600

    calculate done in manual modality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,31 @@
 // Get Calculator Button
 const calculateBtn = document.getElementById('calculate-btn');
+
+// Add Event in calculate button
 calculateBtn.addEventListener('click', event => {
+    // Get total income
     const totalIncome = document.getElementById('total-income');
+
+    // Get all of expenses
     const foodCost = document.getElementById('food-cost');
     const rentCost = document.getElementById('rent-cost');
     const otherCost = document.getElementById('other-cost');
-    const expense = parseFloat(foodCost.value) + parseFloat(rentCost.value) + parseFloat(otherCost.value);
 
-    const totalExpense = document.getElementById('total-expense');
-    totalExpense.innerText = expense;
+    // Summation of all expense
+    const totalExpense = parseFloat(foodCost.value) + parseFloat(rentCost.value) + parseFloat(otherCost.value);
 
-    const balance = document.getElementById('balance');
-    balance.innerText = totalIncome - totalExpense;
+    // Get total expense area
+    const displayExpense = document.getElementById('total-expense');
+    displayExpense.innerText = totalExpense;
 
+    // Get balance
+    const balanceArea = document.getElementById('balance');
+    const balance = parseFloat(totalIncome.value) - totalExpense;
+    balanceArea.innerText = balance;
+
+    // Remaining balance
+    const remainingBalanceArea = document.getElementById('remaining-balance');
+    remainingBalanceArea.innerText = balance;
 })
+
+
